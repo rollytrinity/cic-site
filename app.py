fcfd6589252fa316c1e40a5eda2330b577199481
Trinity Gulland <gullandtrinity@gmail.com>
2024-11-07 15:16:55 +0000

    test

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__,template_folder='./templates',static_folder='./static')
 def home():
     return render_template('home.html')
 
+@app.route('/teachers')
+def teachers():
+    return render_template('teachers.html')
 
 
